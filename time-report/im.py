f0114e61dc689aa537cc6b98178201a9f6ac1dbd
Pedro Rodrigues <prodrigues1990@gmail.com>
2015-12-12 17:02:24 +0100

    Added navigateToDate function

--- time-report/im.py
+++ time-report/im.py
@@ -1,5 +1,7 @@
 import getpass
 from selenium.webdriver.common.keys import Keys
+import re
+import time
 
 def login(driver, username="", password=""):
     if "Login" in driver.title:
@@ -35,11 +37,66 @@ def login(driver, username="", password=""):
     # this is not the login page
     return "An error occurred in function login(): wrong page?"
 
-def navigate(driver, page):
+def navigateToLink(driver, page):
     try:
+        
         el = driver.find_element_by_link_text(page)
         el.click()
+        
         return driver
     except Exception as e:
-        return "An error occurred in function navigate(): sure link '" + page + "' exists?"
+        return "An error occurred in function navigateToLink(): sure link '" + page + "' exists?"
+
+def navigateToDate(driver, calendar, date):
+    try:
+
+        calendar_actions = {
+            "report": {},
+            "planning": {}
+        }
+        calendar_actions["report"] = {
+            "move_left": "MyMatrix_ctl07_wcReportacaoHoras_500",
+            "move_right": "MyMatrix_ctl07_wcReportacaoHoras_502",
+            "day_picker": "MyMatrix_ctl07_wcReportacaoHoras_512"
+        }
+
+        # check year, by picking a day
+        print("selecting year")
+        currentDate = __touchDayOne(driver, calendar_actions[calendar]["day_picker"])
+        while (currentDate.group("ano") != date.split("-")[2]):
+            # move
+            direction = "move_left" if (currentDate.group("ano") > date.split("-")[2]) else "move_right"
+            driver.find_element_by_id(calendar_actions[calendar][direction]).click()
+            # recap
+            currentDate = __touchDayOne(driver, calendar_actions[calendar]["day_picker"])
+
+        print("selecting month")
+        while (currentDate.group("mes") != date.split("-")[1]):
+            # move
+            direction = "move_left" if (currentDate.group("mes") > date.split("-")[1]) else "move_right"
+            driver.find_element_by_id(calendar_actions[calendar][direction]).click()
+            # recap
+            currentDate = __touchDayOne(driver, calendar_actions[calendar]["day_picker"])
+
+        # pick day
+        days = driver.find_element_by_id(calendar_actions[calendar]["day_picker"])
+        days = days.find_elements_by_class_name("date_day")
+        for day in days:
+            if day.text == date.split("-")[0]:
+                day.click()
+                break
         
+    except Exception as e:
+        print(e)
+        return "An error occurred in function navigateToDate(): sure day '" + date + "' exists?"
+
+
+
+
+def __touchDayOne(driver, element_id):
+    # find and touch first day
+    days = driver.find_element_by_id(element_id)
+    days.find_elements_by_class_name("date_day")[0].click()
+    # regex parse the date
+    currentDate = re.compile(r".*: (?P<dia>[\d]{1,2})-(?P<mes>[\d]{1,2})-(?P<ano>[\d]{4})")
+    return currentDate.search(driver.find_element_by_id("MyMatrix_ctl07_lblEditarDia").text)
