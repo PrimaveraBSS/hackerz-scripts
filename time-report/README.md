53ffb9f4e54bd4d05a435a3ed3259f530087cdfa
Pedro Rodrigues <prodrigues1990@gmail.com>
2015-12-14 00:07:00 +0100

    report de despeas a funcionar para multiplas despesas e com todas as combinacoes, validados Lunch e Gasoline

--- time-report/README.md
+++ time-report/README.md
@@ -12,7 +12,20 @@
                 "dia": "[\d]{1,2}-[\d]{1,2}-[\d]{4}",
                 "projecto": ".*",
                 "actividade": ".*",
-                "horas": "7|3.5"
+                "horas": "7|3.5",
+				"expenses": [
+					"tipo": "Another Meal|Diesel|Gasoline|Hotels|Islands-Another meal|Islands-Gasoline|Islands-Lunch working day|Islands-Taxis|Kms in proper car|Lunch working Day|Mobility|Night Out|Other Expenses (detail)|Parking|Taxi Primavera (Sr. Marco)|Taxis|Tolls gate",
+					"local": "Ilhas Açores|Ilhas Madeira|Intracomunitário Espanha|Intracomunitário Outros|Nacional|Outros Mercados",
+					"itenerario": ".*",
+					"diaDeslocao": ".*",
+					"kms": "",
+					"proposito": "",
+					"matricula": ".*",
+					"regiao": "Outros|ROA|ES|AO|MZ|PT",
+					"moeda": "AED|AZK|CVE|EUR|MZN|USD",
+					"valor": "[\d]+.[\d]{2}",
+					"observacoes": ".*"
+				]
             }
         ]
     }
@@ -23,6 +36,8 @@
 
 `login`.`password`: A senha de login no IM (se omitido será pedido na consola)
 
+`time`: lista de tempos e despesas associadas a reportar
+
 `time`.`dia`: Dia a reportar no exacto formato que está no IM. Deve validar com a expressão regular `[\d]{1,2}-[\d]{1,2}-[\d]{4}`
 
 `time`.`projecto`: Nome do projecto a reportar, exactamente como fica visível na combobox do site (usa `time-report-activity-list.py` para obter uma lista de projectos disponíveis)
@@ -30,3 +45,27 @@
 `time`.`actividade`: Nome da actividade a reportar, exactamente como fica visível na combobox do site (usa `time-report-activity-list.py` para obter uma lista de actividades disponíveis para cada projecto)
 
 `time`.`horas`: 7 ou 3.5, conforme especificação da CSU
+
+`time`.`expenses`: lista de despesas a reportar para o dia
+
+`time`.`expenses`.`tipo`: Tipo da despesa, exactamente como fica visível na combobox do site
+
+`time`.`expenses`.`local`: Local da despesa exactamente como fica visível na combobox do site
+
+`time`.`expenses`.`itenerario`: Disponível apenas para o tipo 'Kms in proper car'
+
+`time`.`expenses`.`diaDeslocao`: Disponível apenas para o tipo 'Kms in proper car'
+
+`time`.`expenses`.`kms`: Disponível apenas para o tipo 'Kms in proper car'
+
+`time`.`expenses`.`proposito`: Disponível apenas para o tipo 'Kms in proper car'
+
+`time`.`expenses`.`matricula`: Disponível apenas para os tipos 'Diesel', 'Gasoline', 'Islands-Gasoline', 'Kms in proper car', 'Parking', 'Tolls gate'
+
+`time`.`expenses`.`regiao`:  Disponível apenas para os tipos 'Mobility', 'Night Out'
+
+`time`.`expenses`.`moeda`: Moeda da despesa
+
+`time`.`expenses`.`valor`: Valor original da despesa
+
+`time`.`expenses`.`observacoes`: (Opcional) Obsercações da despesa
--- time-report/time-report.py
+++ time-report/time-report.py
@@ -4,6 +4,75 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 import json
 import getpass
+from time import sleep
+
+#
+#
+#
+def moveToDate(driver, calendar, date):
+    cldr_acts = {
+        "report": {},
+        "expenses": {}
+    }
+    cldr_acts["report"] = {
+        "mv_lft": "MyMatrix_ctl07_wcReportacaoHoras_500",
+        "mv_rht": "MyMatrix_ctl07_wcReportacaoHoras_502",
+        "dy_pck": "MyMatrix_ctl07_wcReportacaoHoras_512",
+        "dy_lbl": "MyMatrix_ctl07_lblEditarDia",
+        "dy_lbl_start": "Editar Dia: "
+    }
+    cldr_acts["expenses"] = {
+        "mv_lft": "MyMatrix_ctl07_wcRegistoDespesas_500",
+        "mv_rht": "MyMatrix_ctl07_wcRegistoDespesas_502",
+        "dy_pck": "MyMatrix_ctl07_wcRegistoDespesas_512",
+        "dy_lbl": "MyMatrix_ctl07_lblTitulo",
+        "dy_lbl_start": "Despesas para o dia: "
+    }
+
+    # verifica se esta no dia a reportar
+    elem = driver.find_element_by_id(cldr_acts[calendar]["dy_lbl"])
+    if not elem.text == cldr_acts[calendar]["dy_lbl_start"] + date:
+        
+        # move pare o ano a reportar
+        while (date.split("-")[2] != elem.text.replace(cldr_acts[calendar]["dy_lbl_start"], "").split("-")[2]):
+            if (date.split("-")[2] > elem.text.replace(cldr_acts[calendar]["dy_lbl_start"], "").split("-")[2]):
+                # ano a reportar maior que ano posicionado, move para a direita
+                elem = driver.find_element_by_id(cldr_acts[calendar]["mv_rht"])
+                elem.click()
+            else:
+                # ano a reportar menor que ano posicionado, move para a esquerda
+                elem = driver.find_element_by_id(cldr_acts[calendar]["mv_lft"])
+                elem.click()
+
+            elem = driver.find_element_by_id(cldr_acts[calendar]["dy_pck"])
+            elem.find_elements_by_class_name("date_day")[0].click()
+            elem = driver.find_element_by_id(cldr_acts[calendar]["dy_lbl"])
+
+            
+        # move pare o mes a reportar
+        while (date.split("-")[1] != elem.text.replace(cldr_acts[calendar]["dy_lbl_start"], "").split("-")[1]):
+            if (date.split("-")[1] > elem.text.replace(cldr_acts[calendar]["dy_lbl_start"], "").split("-")[1]):
+                # mes a reportar maior que mes posicionado, move para a direita
+                elem = driver.find_element_by_id(cldr_acts[calendar]["mv_rht"])
+                elem.click()
+            else:
+                # mes a reportar menor que mes posicionado, move para a esquerda
+                elem = driver.find_element_by_id(cldr_acts[calendar]["mv_lft"])
+                elem.click()
+
+            elem = driver.find_element_by_id(cldr_acts[calendar]["dy_pck"])
+            elem.find_elements_by_class_name("date_day")[0].click()
+            elem = driver.find_element_by_id(cldr_acts[calendar]["dy_lbl"])
+
+    # seleciona o dia
+    elem = driver.find_element_by_id(cldr_acts[calendar]["dy_pck"])
+    elem = elem.find_elements_by_class_name("date_day")
+    for dayElem in elem:
+        if dayElem.text == date.split("-")[0]:
+            dayElem.click()
+            break
+
+
 
 ## load data
 with open("time-report.json") as data_file:
@@ -31,8 +100,6 @@ elem.send_keys(password)
 # login
 elem.send_keys(Keys.RETURN)
 
-
-
 ## navigate to time report page
 assert "My Initiative Management" in driver.title
 elem = driver.find_element_by_link_text("Time Report")
@@ -41,48 +108,7 @@ elem.send_keys(Keys.RETURN)
 ## time report
 assert "Time Report" in driver.title
 for times in data["time"]:
-    # verifica se esta no dia a reportar
-    elem = driver.find_element_by_id("MyMatrix_ctl07_lblEditarDia")
-    if not elem.text == "Editar Dia: " + times["dia"]:
-        
-        # move pare o ano a reportar
-        while (times["dia"].split("-")[2] != elem.text.replace("Editar Dia: ", "").split("-")[2]):
-            if (times["dia"].split("-")[2] > elem.text.replace("Editar Dia: ", "").split("-")[2]):
-                # ano a reportar maior que ano posicionado, move para a direita
-                elem = driver.find_element_by_id("MyMatrix_ctl07_wcReportacaoHoras_502")
-                elem.click()
-            else:
-                # ano a reportar menor que ano posicionado, move para a esquerda
-                elem = driver.find_element_by_id("MyMatrix_ctl07_wcReportacaoHoras_500")
-                elem.click()
-
-            elem = driver.find_element_by_id("MyMatrix_ctl07_wcReportacaoHoras_512")
-            elem.find_elements_by_class_name("date_day")[0].click()
-            elem = driver.find_element_by_id("MyMatrix_ctl07_lblEditarDia")
-
-            
-        # move pare o mes a reportar
-        while (times["dia"].split("-")[1] != elem.text.replace("Editar Dia: ", "").split("-")[1]):
-            if (times["dia"].split("-")[1] > elem.text.replace("Editar Dia: ", "").split("-")[1]):
-                # mes a reportar maior que mes posicionado, move para a direita
-                elem = driver.find_element_by_id("MyMatrix_ctl07_wcReportacaoHoras_502")
-                elem.click()
-            else:
-                # mes a reportar menor que mes posicionado, move para a esquerda
-                elem = driver.find_element_by_id("MyMatrix_ctl07_wcReportacaoHoras_500")
-                elem.click()
-
-            elem = driver.find_element_by_id("MyMatrix_ctl07_wcReportacaoHoras_512")
-            elem.find_elements_by_class_name("date_day")[0].click()
-            elem = driver.find_element_by_id("MyMatrix_ctl07_lblEditarDia")
-
-    # seleciona o dia
-    elem = driver.find_element_by_id("MyMatrix_ctl07_wcReportacaoHoras_512")
-    elem = elem.find_elements_by_class_name("date_day")
-    for dayElem in elem:
-        if dayElem.text == times["dia"].split("-")[0]:
-            dayElem.click()
-            break
+    moveToDate(driver, "report", times["dia"])
 
     # verfica se não tem report neste dia
     elem = driver.find_element_by_id("MyMatrix_ctl07_txtTotalHoras")
@@ -99,8 +125,82 @@ for times in data["time"]:
     elem = driver.find_element_by_id("MyMatrix_ctl07_txtHours")
     elem.send_keys(times["horas"])
 
-    # grava o report, passa ao próximo ou fecha o browser
+    # grava o report, passa ao próximo
     elem = driver.find_element_by_id("MyMatrix_ctl07_btnGravar")
-    #elem.click()
-    
+    elem.click()
+
+    # reporta despesas
+    if "expenses" in times:
+        # navigate to expense page 
+        elem = driver.find_element_by_link_text("Expenses Report")
+        elem.send_keys(Keys.RETURN)
+            
+        for expense in times["expenses"]:
+            moveToDate(driver, "expenses", times["dia"])
+
+            # tipo
+            select = Select(driver.find_element_by_id("MyMatrix_ctl07_ddlTipoDespesa"))
+            select.select_by_visible_text(expense["tipo"])
+
+            # local
+            select = Select(driver.find_element_by_id("MyMatrix_ctl07_ddlLocal"))
+            select.select_by_visible_text(expense["local"])
+
+            # specifics
+            tipos = {"Diesel", "Gasoline", "Islands-Gasoline", "Kms in proper car"}
+            if (expense["tipo"] in tipos):
+                # matricula
+                elem = driver.find_element_by_id("MyMatrix_ctl07_txtMatriculaOnly")
+                elem.send_keys(expense["matricula"])
+
+            tipos = {"Kms in proper car"}
+            if (expense["tipo"] in tipos):
+                # itenerario
+                elem = driver.find_element_by_id("MyMatrix_ctl07_txtDeslocacao")
+                elem.send_keys(expense["itenerario"])
+                
+                # diaDeslocao
+                elem = driver.find_element_by_id("MyMatrix_ctl07_txtDiaDeslocacao")
+                elem.send_keys(expense["diaDeslocao"])
+                
+                # kms
+                elem = driver.find_element_by_id("MyMatrix_ctl07_txtKms")
+                elem.send_keys(expense["kms"])
+                
+                # proposito
+                elem = driver.find_element_by_id("MyMatrix_ctl07_txtCausas")
+                elem.send_keys(expense["proposito"])
+
+            tipos = {"Mobility"}
+            if (expense["tipo"] in tipos):
+                # regiao
+                select = Select(driver.find_element_by_id("MyMatrix_ctl07_ddlRegiao"))
+                select.select_by_visible_text(expense["regiao"])
+                
+                
+
+            # valor !should not be last, otherwise selenium won't wait for exchange recal!
+            elem = driver.find_element_by_id("MyMatrix_ctl07_txtValorOriginal")
+            elem.send_keys(expense["valor"])
+
+            # observacoes
+            if "observacoes" in expense:
+                elem = driver.find_element_by_id("MyMatrix_ctl07_txtObservacoes")
+                elem.send_keys(expense["observacoes"])
+
+            # moeda
+            select = Select(driver.find_element_by_id("MyMatrix_ctl07_ddlMoeda"))
+            select.select_by_visible_text(expense["moeda"])
+
+            # gravar                
+            elem = driver.find_element_by_id("MyMatrix_ctl07_btnGravar")
+            elem.click()
+
+            # ler id da despesa inserida
+
+        ## navigate back to time report page
+        elem = driver.find_element_by_link_text("Time Report")
+        elem.send_keys(Keys.RETURN)
+
+# close driver and exit
 driver.close()
